b25970d5576592b503326d86c46a0ddc60dd8e72
Ruj <rutkiller@gmail.com>
2021-02-01 09:05:54 +0700

    Delete and Up-new-File

--- e-smart/e_learning/login/templates/footer.php
+++
@@ -1,8 +0,0 @@
-</body>
-</html>
-
-<script src="../assets/js/font-awesome.min.js" crossorigin="anonymous"></script>
-<script src="../assets/js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-<script src="../assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-
-<?php //sqlsrv_close($conn); ?>
--- e-smart/title.php
+++
@@ -1,2 +0,0 @@
-  <title>TDA E-SMART</title>
-  <link rel="icon" href="../img/icon/e_smart.ico" type="image/x-icon">
